95849538b6d239e47a8b2d84de47682254863ed5
knapalke <knapalke@gmail.com>
2023-05-16 20:39:25 +0200

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,6 @@
 import Home from "@components/Home";
 
-const getFighterData = async () => {
-	const url = "https://mma-stats.p.rapidapi.com/May_13_2023";
-	const options = {
-		method: "GET",
-		headers: {
-			"X-RapidAPI-Key": "daa98300a3mshdce5fa79575d577p1b4ceejsn540444249dcb",
-			"X-RapidAPI-Host": "mma-stats.p.rapidapi.com",
-		},
-	};
-
-	try {
-		const response = await fetch(url, options);
-		const result = await response.text();
-		return result;
-	} catch (error) {
-		console.error(error);
-	}
-};
 const page = async () => {
-	const data = await getFighterData();
-	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<Home></Home>
-		</main>
-	);
+	return <Home></Home>;
 };
 export default page;
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Footer = () => {
-	return <div>Footer</div>;
+	return (
+		<footer className="footer footer-center p-4 bg-base-300 text-base-content w-screen">
+			<div>
+				<p>Copyright © 2023 - Kamil Knap</p>
+			</div>
+		</footer>
+	);
 };
 
 export default Footer;
