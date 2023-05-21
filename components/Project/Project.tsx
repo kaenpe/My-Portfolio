da0591c9cfe1165228398b2b0576aedfe0380496
knapalke <knapalke@gmail.com>
2023-05-21 17:13:27 +0200

    starting projects

--- components/Project/Project.tsx
+++ components/Project/Project.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ProjectElement from "./ProjectElement";
 
 const Project = () => {
-	return <div>log</div>;
+	return <div className="hero min-h-screen w-screen bg-base-200">log</div>;
 };
 
 export default Project;
