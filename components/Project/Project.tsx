bf10a2d7fca15907952ddb7e28c6942d28fceba0
knapalke <knapalke@gmail.com>
2023-05-23 23:20:04 +0200

    stretching works, animation

--- components/Project/Project.tsx
+++ components/Project/Project.tsx
@@ -5,19 +5,24 @@ import Image from "next/image";
 const Project = () => {
 	return (
 		<div className="hero min-h-screen bg-base-200 w-screen py-32 px-10">
-			<h1>MY PROJECTS</h1>
 			<div className="hero-content grid grid-cols-2 place-items-center min-h-screen w-full">
-				<ProjectElement text={"yo"} img="proompta" title={"Proompta"} />
+				<ProjectElement
+					text={"yo"}
+					img="proompta"
+					title={"Proompta"}
+					element={1}
+				/>
 				<ProjectElement
 					text={"yo"}
 					img="burger-builder"
 					title={"Burger Maker"}
+					element={2}
 				/>
 				<ProjectElement
 					text={"yo"}
 					img="plumtree"
 					title={"Plumtree Agency"}
-					isThird
+					element={3}
 				/>
 			</div>
 		</div>
--- components/Project/ProjectElement.tsx
+++ components/Project/ProjectElement.tsx
@@ -1,23 +1,35 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-
+import { motion } from "framer-motion";
 interface IProps {
 	title: string;
 	text: string;
 	link: string;
 	img: string;
-	isThird: boolean;
+	element: number;
 }
 
-const ProjectElement = ({ title, text, link, img, isThird }: IProps) => {
+const ProjectElement = ({ title, text, link, img, element }: IProps) => {
+	const wrapper = {
+		initial: {
+			x: element === 1 ? -50 : element === 2 ? 50 : element === 3 && 0,
+			y: element === 3 && 150,
+			opacity: 0,
+		},
+		animate: { x: 0, y: 0, opacity: 1, transition: { duration: 1 } },
+	};
 	return (
-		<div
-			className={`grid grid-rows-2 h-full w-full justify-center items-center border border-solid border-black p-3 shadow-xl rounded-lg  ${
-				isThird && "col-span-2 w-full"
+		<motion.div
+			initial="initial"
+			animate="animate"
+			variants={wrapper}
+			className={`grid grid-rows-2 grid-cols-2 h-full w-full justify-center items-center border border-solid border-black p-3 shadow-xl rounded-lg  ${
+				element === 3 && "col-span-2 w-full"
 			}`}
 		>
-			<div className="relative w-full h-full">
+			<div className="relative w-full h-full col-span-2">
 				<Image
 					alt="proompta"
 					src={`/assets/projects/${img}.png`}
@@ -27,12 +39,12 @@ const ProjectElement = ({ title, text, link, img, isThird }: IProps) => {
 				/>
 			</div>
 
-			<div className="prose text-center">
+			<div className="prose text-center col-span-2">
 				<h1 className="text-5xl font-bold m-0 mt-5">{title}</h1>
 				<p className="py-6 text-left">{text}</p>
 				<button className="btn btn-primary">Get Started</button>
 			</div>
-		</div>
+		</motion.div>
 	);
 };
 
